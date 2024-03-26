3bd00b831fc2ce71d15c8665baef80f51edf3d87
Will MacLean <maclean.will@gmail.com>
2024-03-26 11:41:51 -0500

    games

--- raterapi/models/game.py
+++ raterapi/models/game.py
@@ -5,22 +5,10 @@ class Game(models.Model):
     title = models.CharField(max_length=155)
     description = models.CharField(max_length=155)
     designer = models.CharField(max_length=155)
-    year_released = models.IntegerField(max_length=4)
-    players = models.IntegerField(max_length=2)
-    completion_time = models.IntegerField(max_length=2)
+    year_released = models.IntegerField()
+    players = models.IntegerField()
+    completion_time = models.IntegerField()
     min_age = models.ImageField(max_length=2)
 
 
 
-
-
-#     Table Games {
-#   id int pk
-#   title varchar
-#   description varchar
-#   designer varchar
-#   year_released number
-#   players int
-#   completion_time number
-#   age number 
-# }
